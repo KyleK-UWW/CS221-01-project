c44bcc3ff993c04efb22e781452ecd4ec2153aed
KyleK <kylekleinschmidt1989@gmail.com>
2025-03-02 12:54:19 -0600

    first commit

---
+++ Jukebox.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel.Design;
+using Jukebox_Project;
+
+public static class Jukebox
+{
+    private static int GetLineCount(string path)
+    {
+        if (!File.Exists(path))
+        {
+            throw new FileNotFoundException("cannot find file", path);
+        }
+        int count = 0;
+        using StreamReader reader = new StreamReader(path);
+        while (!reader.EndOfStream)
+        {
+            reader.ReadLine();
+            count++;
+        }
+        return count;
+    }
+    private static void Main()
+    {
+        string path = "Song-List.csv";//.csvfile not audio!
+        int lineCount = GetLineCount(path);
+
+        Song[] playlist = new Song[lineCount - 1];
+
+        try
+        {
+            using StreamReader reader = new StreamReader(path);
+            reader.ReadLine();
+            for (int i = 0; i < playlist.Length; i++)
+            {
+                string line = reader.ReadLine();
+                string[] cols = line.Split(',');
+
+                string title = cols[0];
+                double playtime = double.Parse(cols[1]);
+                string artist = cols[2];
+                int releaseYear = int.Parse(cols[3]);
+                string genre = cols[4];
+                int songNum = int.Parse(cols[5]);//or playslist[i]+1? would like to refernce array index for value
+                int timesPlayed = 0; //initial setting for tracking song history..
+
+                playlist[i] = new Song(title, artist, genre, releaseYear, playtime,songNum,timesPlayed);
+
+            }
+
+
+        }
+        catch
+        {
+            Console.WriteLine("Error reading from file", path);
+
+            return;
+
+        }
+
+        Console.WriteLine("Current playlist:");
+        for (int i = 0; i < playlist.Length; i++) 
+        {
+            Console.WriteLine($"{playlist[i].SongNum} + \t{playlist[i]}");
+
+        }
+
+        Console.WriteLine("\nPlease enter Song Number you wish to play:");
+
+        string input = Console.ReadLine();
+        int selection = int.Parse(input);
+
+        for (int i = 0;i < playlist.Length;i++)
+        {
+            if (selection == playlist[i].SongNum)
+            {
+                //song trigger happens here inside if{}
+                Console.WriteLine($"\nNow playing:\t{playlist[i]}");
+                playlist[i].TimesPlayed++;
+                //stretch goal for screen color change based on song genre would happen here...
+            }
+            else
+            {
+                throw new Exception("Invalid Song selection, please try again!");
+            }
+        }
+
+       
+    }
+
+
+}
---
+++ Song.cs
@@ -0,0 +1,117 @@
+﻿
+
+namespace Jukebox_Project
+{
+    public class Song
+    {
+        string title, artist, genre;
+        int releaseYear, songNum, timesPlayed;
+        double playtime;
+
+        public Song(string title, string artist, string genre, int releaseYear, double playtime, int songNum, int timesPlayed)
+        {
+            Title = title;
+            Artist = artist;
+            Genre = genre;
+            ReleaseYear = releaseYear;
+            Playtime = playtime;
+            SongNum = songNum;
+            TimesPlayed = timesPlayed;
+            
+        }
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (string.IsNullOrEmpty(title))
+                {
+                    throw new Exception("title cannot be null");
+                }
+                title = value;
+            }
+
+        }
+        public string Artist
+        {
+            get => artist;
+            set
+            {
+                if (string.IsNullOrEmpty(artist))
+                {
+                    throw new Exception("artist cannot be null");
+                }
+                artist = value;
+            }
+        }
+
+        public string Genre
+        {
+            get => genre;
+            set
+            {
+                if (string.IsNullOrEmpty(genre))
+                {
+                    throw new Exception("genre cannot be null");
+                }
+                genre = value;
+            }
+        }
+        public int ReleaseYear
+        {
+            get => releaseYear;
+            set
+            {
+                if (releaseYear < 1950 || releaseYear > DateTime.Now.Year)
+                {
+                    throw new Exception("Invalid release year");
+                }
+                releaseYear = value;
+            }
+
+        }
+        public int SongNum
+        {
+            get => songNum;
+            set
+            {
+                if (songNum < 0)
+                {
+                    throw new Exception("song number cannot be negative");
+                }
+                songNum = value;
+            }
+        }
+        public int TimesPlayed
+        {
+            get => timesPlayed;
+            set
+            {
+                if(timesPlayed < 0)
+                {
+                    throw new Exception("invalid times played entry");
+                }
+                timesPlayed = value;
+            }
+        }
+        public double Playtime
+        {
+            get => playtime;
+            set
+            {
+                if (playtime == 0)
+                {
+                    throw new Exception("Song playtime cannot be zero");
+                }
+                playtime = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} performed by {Artist} in {ReleaseYear}. ({Playtime}) Category: {Genre}";
+        }
+
+    }
+}
